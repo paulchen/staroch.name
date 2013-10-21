4e6844316479e6830831026d81bfbddd5331f9e2
paulchen <paulchen@rueckgr.at>
2013-10-21 17:37:38 +0200

    update

--- web/index.php
+++ web/index.php
@@ -9,11 +9,24 @@ else {
 	$lang = $_REQUEST['lang'];
 }
 
-$templates_dir = dirname(__FILE__) . '/../templates';
-if(!file_exists("$templates_dir/index_$lang.php")) {
-	header("Location: /$default_lang/");
-	die();
+switch($_REQUEST['request']) {
+	case '':
+		$templates_dir = dirname(__FILE__) . '/../templates';
+		if(!file_exists("$templates_dir/index_$lang.php")) {
+			header("Location: /$default_lang/");
+			die();
+		}
+
+		require_once("$templates_dir/index.php");
+		break;
+
+	default:
+		http_send_status(404);
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, 'https://' . $_SERVER['SERVER_NAME'] . '/XXX');
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$data = str_replace('/XXX', $_SERVER['REQUEST_URI'], $data);
+		curl_close($curl);
+		die($data);
 }
-
-require_once("$templates_dir/index.php");
-
