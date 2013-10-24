8b6c34b669d379df6f7b34817f52ac15ca909275
paulchen <paulchen@rueckgr.at>
2013-10-24 21:35:03 +0200

    updated English text

--- templates/index_en.php
+++ templates/index_en.php
@@ -1,6 +1,46 @@
 <div class="language"><a href="<?php echo $language_links['de']; ?>" title="Deutsch"><img alt="Deutsch" src="/images/de.png" /></a></div>
 <h1>Paul Staroch</h1>
 <div class="degree">Dipl.-Ing. Paul Staroch, BSc</div>
-<div>
-	TODO (English)
+<div class="introduction">
+	<p>
+	Alumnus from <a href="http://www.tuwien.ac.at/en/tuwien_home/">Vienna University of Technology</a>. Finished studies:
+	<ul>
+		<li><a href="http://www.informatik.tuwien.ac.at/lehre/studien/bachelor/software-and-information-engineering">Software &amp; Information Engineering</a> (BSc),</li>
+		<li><a href="http://www.informatik.tuwien.ac.at/lehre/studien/master/software-engineering-and-internet-computing">Software Engineering &amp; Internet Computing</a> (Dipl.-Ing., equivalent to MSc).</li>
+	</ul>
+	</p>
+
+	<p>
+	Currently seeking for a job. Previous work experience:
+	<ul>
+		<li><a href="http://www.mediaxp.de/en/">MediaXP GmbH</a> (IT staff),</li>
+		<li><a href="http://www.tuwien.ac.at/en/tuwien_home">Vienna University of Technology</a> (Tutor),</li>
+		<li><a href="http://www.fortuna-swa.at/home/haeuser/appartementhaus">Appartementhaus Fortuna</a> (Civilian service).</li>
+	</ul>
+	</p>
+
+	<p>
+	Member of the organizational team of <a href="http://huntu.at/">hunTU</a>, the &quot;scavenger hunt that is a bit different&quot;.
+	<br />
+	Moderator at <a href="http://www.informatik-forum.at/">Informatik-Forum.at</a>.
+	</p>
+
+	<p>
+	Operator of a server named <b>rueckgr.at</b> running <a href="https://rueckgr.at/">various services</a> including a <a href="https://tor.rueckgr.at">Tor relay</a> and a <a href="https://jabber.rueckgr.at/">public Jabber server</a>.
+	</p>
+
+	<p>
+	Contact: <a href="mailto:paul@staroch.name">paul@staroch.name</a>, <a href="mailto:paulchen@rueckgr.at">paulchen@rueckgr.at</a>
+	</p>
+
+	<hr />
+
+	<p>
+	The master thesis (&quot;A Weather Ontology for Predictive Control in Smart Homes&quot;) is available for download:
+	<ul>
+		<li><a href="/thesis/thesis.pdf">Master thesis</a></li>
+		<li><a href="/thesis/poster.pdf">Poster</a></li>
+		<li><a href="/thesis/presentation.pdf">Presentation slides</a></li>
+	</ul>
+	</p>
 </div>
